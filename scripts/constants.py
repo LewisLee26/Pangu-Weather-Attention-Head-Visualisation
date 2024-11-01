742a60150f6893b937795413c1fac5b6e648c965
LewisLee26 <lewis.patrick.lee@gmail.com>
2024-11-01 14:24:58 +0000

    Moved constants.py

---
+++ scripts/constants.py
@@ -0,0 +1,72 @@
+ATTENTION_LAYER_NAMES = [
+    '/b1/Add_output_0', 
+    '/b1/Add_3_output_0',
+    '/b1/Add_7_output_0', 
+    '/b1/Add_10_output_0',
+    '/b1/Add_14_output_0', 
+    '/b1/Add_17_output_0',
+    '/b1/Add_21_output_0', 
+    '/b1/Add_24_output_0',
+    '/b1/Add_28_output_0', 
+    '/b1/Add_31_output_0',
+    '/b1/Add_35_output_0', 
+    '/b1/Add_38_output_0',
+    '/b1/Add_42_output_0', 
+    '/b1/Add_45_output_0',
+    '/b1/Add_49_output_0',
+    '/b1/Add_52_output_0',
+]
+
+POST_SOFTMAX_ATTENTION_LAYER_NAMES = [
+    '/b1/a11/Softmax_output_0',
+    '/b1/a19/Softmax_output_0',
+    '/b1/a29/Softmax_output_0',
+    '/b1/a37/Softmax_output_0',
+    '/b1/a45/Softmax_output_0',
+    '/b1/a53/Softmax_output_0',
+    '/b1/a61/Softmax_output_0',
+    '/b1/a68/Softmax_output_0',
+    '/b1/a77/Softmax_output_0',
+    '/b1/a85/Softmax_output_0',
+    '/b1/a93/Softmax_output_0',
+    '/b1/a101/Softmax_output_0',
+    '/b1/a109/Softmax_output_0',
+    '/b1/a117/Softmax_output_0',
+    '/b1/a128/Softmax_output_0',
+    '/b1/a136/Softmax_output_0',
+]
+
+PRE_EARTH_POS_BIAS_ATTENTION_LAYER_NAMES = [
+    '/b1/MatMul_output_0',
+    '/b1/MatMul_2_output_0',
+    '/b1/MatMul_4_output_0',
+    '/b1/MatMul_6_output_0',
+    '/b1/MatMul_8_output_0',
+    '/b1/MatMul_10_output_0',
+    '/b1/MatMul_12_output_0',
+    '/b1/MatMul_14_output_0',
+    '/b1/MatMul_16_output_0',
+    '/b1/MatMul_18_output_0',
+    '/b1/MatMul_20_output_0',
+    '/b1/MatMul_22_output_0',
+    '/b1/MatMul_24_output_0',
+    '/b1/MatMul_26_output_0',
+    '/b1/MatMul_28_output_0',
+    '/b1/MatMul_30_output_0',
+]
+
+NO_WIN_SHIFT_ATTENTION_LAYER_INDEXES = [
+    0,2,4,6,8,10,12,14,
+]
+
+WIN_SHIFT_ATTENTION_LAYER_INDEXES = [
+    1,3,5,7,9,11,13,15,
+]
+
+UP_SAMPLE_ATTENTION_LAYER_INDEXES = [
+    0,1,14,15
+]
+
+DOWN_SAMPLE_ATTENTION_LAYER_INDEXES = [
+    2,3,4,5,6,7,8,9,10,11,12,13,
+]
